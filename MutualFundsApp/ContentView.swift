c098b176fa91edd9697983ba3a377ec035ef89b2
vansh sharma <vanshsharma5503@gmail.com>
2025-08-20 14:52:41 +0530

    Initial Commit

---
+++ MutualFundsApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MutualFundsApp
+//
+//  Created by Vansh Sharma on 20/08/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MutualFundsApp/MutualFundsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MutualFundsAppApp.swift
+//  MutualFundsApp
+//
+//  Created by Vansh Sharma on 20/08/25.
+//
+
+import SwiftUI
+
+@main
+struct MutualFundsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
